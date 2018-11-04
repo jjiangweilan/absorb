626860c921644858c664d4ac3d2bd50337492f94
weilan <jjiangweilan@gmail.com>
2018-11-03 22:16:56 -0700

    add new tiles, camera controll, etc still need to work on enemy movment

---
+++ Assets/Script/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public Transform target;
+    public float smooth;
+
+    private Vector3 offset;
+    // Update is called once per frame
+    void Update () {
+    }
+
+    void LateUpdate()
+    {
+        offset = Vector3.zero; //this should be adjusted depending on if player is moving right or left
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smooth * Time.smoothDeltaTime);
+        smoothedPosition.z = transform.position.z;
+        Debug.Log(smoothedPosition);
+        transform.position = smoothedPosition;
+    }
+}
---
+++ Assets/Script/Enemy/BasicMonsterController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMonsterController : MonoBehaviour {
+    // Use this for initialization
+    [SerializeField] float platformTestLendth;
+    [SerializeField] Collider2D collider_;
+    void Start () {
+        //GetComponent<Rigidbody2D>().velocity = new Vector2(3,0);
+    }
+
+    // Update is called once per frame
+    void Update () {
+    }
+
+    bool isOnPlatform()
+    {
+        float direction = Mathf.Sign(transform.localScale.x);
+
+        var start = collider_.bounds.center;
+        start.y -= collider_.bounds.size.y / 2.0f - 0.01f;
+        if (direction == 1)
+        {
+            start.x += collider_.bounds.center.x / 2.0f;
+        }
+        else
+        {
+            start.x -= collider_.bounds.center.x / 2.0f;
+        }
+
+        var rayTest = Physics2D.Raycast(start, Vector2.down, platformTestLendth);
+
+        return rayTest.collider != null;
+    }
+
+    void FixedUpdate()
+    {
+    }
+
+}
